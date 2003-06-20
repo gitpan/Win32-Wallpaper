b40aaaad665980c24e9ff99885660a79e7e91edd
Michael Accardo <accardo@cpan.org>
2003-06-20 03:06:01 +0000

    Import of ACCARDO/Win32-Wallpaper-0.01 from CPAN.
    gitpan-cpan-distribution: Win32-Wallpaper
    gitpan-cpan-version:      0.01
    gitpan-cpan-path:         ACCARDO/Win32-Wallpaper-0.01.tar.gz
    gitpan-cpan-author:       ACCARDO
    gitpan-cpan-maturity:     released

---
+++ Wallpaper/Wallpaper.pm
@@ -0,0 +1,89 @@
+package Win32::Wallpaper;
+require Exporter;
+@Wallpaper::ISA    = qw(Exporter);
+@Wallpaper::EXPORT = qw(wallpaper);
+$VERSION = '0.01';
+
+use Win32::API;
+use Win32::AbsPath qw(RelativeToAbsolute);
+use Win32::TieRegistry(Delimiter=>"/");
+use Image::Size;
+
+sub wallpaper {
+
+    my ( $file, $style ) = @_;
+
+    open( IN, $file ) or die "Cannot open $file: $!";
+    my ( $width, $height, $id ) = imgsize( \*IN );
+    die "Only Windows BitMaP images supported. You used a $id."
+      unless ( $id =~ /bmp/i );
+
+    if ( $style =~ /\btile\b|\bstretch\b|\bcenter\b/i ) {
+        my $opt1 = "CUser/Control Panel/Desktop/TileWallpaper";
+        my $opt2 = "CUser/Control Panel/Desktop/WallpaperStyle";
+
+
+        if ( $style =~ /tile/i ) {
+            $Registry->{$opt1}=1;  
+            $Registry->{$opt2}=0; 
+           
+        }
+        elsif ( $style =~ /stretch/i ) {
+            $Registry->{$opt1}=0;
+            $Registry->{$opt2}=2; 
+           
+        }
+        else {
+            $Registry->{$opt1}=0;
+            $Registry->{$opt2}=0; 
+            
+        }
+    }
+    else {
+        die
+"Your wallpaper style - $style - is unsupported. Try \"Center\" \"Tile\" or \"Stretch\""
+          if $style;
+
+    }
+
+    $file = RelativeToAbsolute $file;
+
+    my $SystemParametersInfo =
+      new Win32::API(qw(user32 SystemParametersInfoA NNPN N)) || die $^E;
+    use constant SPIF_SETDESKWALLPAPER => 20;
+    use constant SPIF_UPDATEINIFILE    => 1;
+    $SystemParametersInfo->Call( SPIF_SETDESKWALLPAPER, 0, $file,
+        SPIF_UPDATEINIFILE ) || die $^E;
+
+}
+
+1;
+__END__
+
+=pod
+
+=head1 NAME
+
+Win32::Wallpaper - Modify Win32 Wallpaper
+
+=head1 SYNOPSIS
+
+	use Win32::Wallpaper;
+
+	wallpaper("image.bmp", "tile");
+
+=head1 DESCRIPTION
+
+With this Win32 module, you can set a new wallpaper image and style. Three styles are supported: center, stretch, and tile. Only Window BitMaP (BMP) images are supported at this time. This module will eventually be obsolete and no longer supported, with the release of my Win32::Desktop module, which will support the ActiveDesktop interface.  
+
+=head1 AUTHOR
+
+Mike Accardo <mikeaccardo@yahoo.com>
+
+=head1 COPYRIGHT
+
+   Copyright (c) 2003, Mike Accardo. All Rights Reserved.
+ This module is free software. It may be used, redistributed
+and/or modified under the terms of the Perl Artistic License
+     (see http://www.perl.com/perl/misc/Artistic.html)
+=cut
